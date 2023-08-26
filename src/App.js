2e05773f9d71fad1a39338b4e45e3411b919ba56
Svetlozar Deskov <s.deskov@gmail.com>
2023-08-26 23:47:47 +0300

    Adding the WS connection; Handling the upper/lowercase requirement to split messages in columns; Adding styling;

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState, useRef } from 'react';
 
 function App() {
+  const [value, setInput] = useState('');
+  const [socket, setSocket] = useState(null);
+  const [responseData, setResponseData] = useState({ capital: [], lower: [] });
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    const ws = new WebSocket('wss://ws.postman-echo.com/raw');
+
+    ws.addEventListener("open", () => {
+      console.log("Connected!");
+    })
+
+    ws.addEventListener('message', ({data}) => {
+      console.log(data);
+      //make a copy of the responseData object
+      const responseDataCopy = { ...responseData };
+      //ignore the empty messages
+      if (data && data.trim() !== "") {
+        if (data.charAt(0) === data.charAt(0).toLowerCase()) {
+          responseDataCopy.lower = [...responseDataCopy.lower, data];
+        } else {
+          responseDataCopy.capital = [...responseDataCopy.capital, data];
+        }
+      }
+
+      setResponseData(responseDataCopy);
+      setInput('');
+
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+
+    })
+
+    setSocket(ws);
+
+    return () => {
+      ws.close();
+    }
+  }, [responseData]);
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(value);
+
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      socket.send(value);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h2>WebSocket Messages:</h2>
+        <div className="row">
+          <div className="column" style={{ backgroundColor: "#67962d" }}>
+            <h4>Capital Letter</h4>
+            {responseData.capital.map((message, m) => (
+              <p key={message + m}>{message}</p>
+            ))}
+          </div>
+          <div className="column" style={{ backgroundColor: "#7eaf3e" }}>
+            <h4>Lowercase Letter</h4>
+            {responseData.lower.map((message, m) => (
+              <p key={message + m}>{message}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+      <h2>WebSocket Echo Form</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          className="input"
+          ref={inputRef}
+          autoFocus
+          type="text"
+          value={value}
+          onChange={handleInput}
+          placeholder="Write a message to send to the socket"
+        />
+        <button className="submit-button" type="submit">Submit</button>
+      </form>
     </div>
   );
 }
